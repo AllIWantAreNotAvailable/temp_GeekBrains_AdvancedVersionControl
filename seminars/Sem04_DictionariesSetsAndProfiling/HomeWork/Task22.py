0984169c2db6436142fa7fc0e350023007e2e201
Momotov Kirill <unknownman@live.ru>
2023-03-24 19:56:46 +0300

    Refactoring of task №22 and №24

--- seminars/Sem04_DictionariesSetsAndProfiling/HomeWork/Task22.py
+++ seminars/Sem04_DictionariesSetsAndProfiling/HomeWork/Task22.py
@@ -1,7 +1,6 @@
 """ Задача №22:
 Даны два неупорядоченных набора целых чисел (может быть, с повторениями). Выдать без повторений
-в порядке возрастания все те числа, которые
-встречаются в обоих наборах.
+в порядке возрастания все те числа, которые встречаются в обоих наборах.
 Пользователь вводит 2 числа.
     N - кол-во элементов первого множества.
     M - кол-во элементов второго множества. Затем пользователь вводит сами элементы множеств.
@@ -12,6 +11,43 @@
 6 12
 """
 
-fst, sec = [2, 4, 6, 8, 10, 12, 10, 8, 6, 4, 2], [3, 6, 9, 12, 15, 18]
-fst, sec = set(fst), set(sec)
-print(sorted(list(fst & sec)))
+
+def get_intersection_of_sets(*args) -> list:
+    """ Функция возвращает пересечения переданных последовательностей.
+
+    :param args: Переданные последовательности.
+    :return: Список из элементов пересечения последовательностей, <class 'int'>
+    """
+    if len(args) == 1:
+        result_of_intersection = list(*args)
+        result_of_intersection.sort()
+        return result_of_intersection
+    try:
+        temp_tuple = tuple(map(set, args))
+    except TypeError as ex:
+        print(ex)
+        return list()
+    else:
+        length = len(temp_tuple)
+        intersection = lambda index: temp_tuple[index - 1] & temp_tuple[index]
+        temp_intersections = map(intersection, range(1, length))
+
+        return get_intersection_of_sets(*temp_intersections)
+
+
+# [ ] Добавить функционал ввода последовательностей пользователем.
+def user_input():
+    pass
+
+
+def main() -> None:
+    """ Главная функция.
+
+    :return: None
+    """
+    fst, sec = [2, 4, 6, 8, 10, 12, 10, 8, 6, 4, 2], [3, 6, 9, 12, 15, 18]
+    print(get_intersection_of_sets(fst, sec))
+
+
+if __name__ == '__main__':
+    main()
--- seminars/Sem04_DictionariesSetsAndProfiling/HomeWork/Task24.py
+++ seminars/Sem04_DictionariesSetsAndProfiling/HomeWork/Task24.py
@@ -18,12 +18,34 @@
 4 -> 1 2 3 4
 9
 """
-
 from random import randint
 
 
-bushes = [randint(1, 10) for _ in range(0, 4)]
-round_seedbed = bushes + bushes[:2]
-berry_harvested = list(map(lambda bush: sum(round_seedbed[bush:bush+3]), range(0, len(round_seedbed))))
-max_berries = max(berry_harvested)
-print(bushes, '->', max_berries)
+def get_max_of_triplets(*args) -> int:
+    """ Функция возвращает максимальное целое значение суммы тройки переданных элементов.
+
+    :param args: Элементы последовательности типа целое число.
+    :return: Максимальное значение суммы трех элементов последовательности, <class 'int'>
+    """
+    length = len(args)
+    sum_of_triplet = lambda bush: sum((args[bush - 2], args[bush - 1], args[bush]))
+    berry_harvested = tuple(map(sum_of_triplet, range(length)))
+    return max(berry_harvested)
+
+
+# [ ] Добавить функционал ввода последовательностей пользователем.
+def user_input():
+    pass
+
+
+def main():
+    """ Главная функция.
+
+    :return: None
+    """
+    bushes = [randint(1, 10) for _ in range(0, 4)]
+    print(bushes, '->', get_max_of_triplets(*bushes))
+
+
+if __name__ == '__main__':
+    main()
